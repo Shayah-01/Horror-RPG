29c62866370cc5d1d91d921f9811984c31e924f6
Shayah-01 <122223761+Shayah-01@users.noreply.github.com>
2024-04-21 20:00:20 +0530

    Window can be closed by pressing Esc Key & clicking the Close button!

--- Source-Code/main.go
+++ Source-Code/main.go
@@ -13,10 +13,10 @@ func main() { // Directly firing the game from func main is not done, Run func i
 func run() {
 
 	cfg := pixelgl.WindowConfig{ // configuring the details of how our window would start
-		Title:  "HRPG by Shayah-01",
-		Bounds: pixel.R(0, 0, 250, 370),
-		// Resizable: true,
-		VSync: true,
+		Title:     "HRPG by Shayah-01",
+		Bounds:    pixel.R(0, 0, 250, 370),
+		Resizable: true,
+		VSync:     true,
 	}
 	win, err := pixelgl.NewWindow(cfg) // Opening the windows with prev. conf. - error if failed
 
@@ -24,9 +24,9 @@ func run() {
 		panic(err) // Will print error & help me know what went wrong if window fails to open
 	}
 
-	// !win.Closed() =  !win.JustPressed(pixelgl.KeyEscape)
+	// !win.Closed() =  !win.JustPressed(pixelgl.KeyEscape) = closing with key esc or mouse close button
 
-	for !win.JustPressed(pixelgl.KeyEscape) { // Keep windows updating for keypresses/events until as long as its NOT closed!
+	for !win.Closed() && !win.JustPressed(pixelgl.KeyEscape) { // Keep windows updating for keypresses/events until as long as its NOT closed!
 		win.Clear(colornames.Rosybrown) // Window will display CLEAR darkgrey space
 		win.Update()
 	}
