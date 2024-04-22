5d12c6a9c988007e5d96544e565933be0608fcb9
Shayah-01 <122223761+Shayah-01@users.noreply.github.com>
2024-04-22 12:03:46 +0530

    Playing with sprite movements, scaling & positioning!
    Learnt to:
    - Load picture as pixel sprite
    - Move the sprite using keys 
    - Scale & position sprite

--- Source-Code/main.go
+++ Source-Code/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"image"
+	"os"
+
+	_ "image/png"
+
 	"github.com/gopxl/pixel"
 	"github.com/gopxl/pixel/pixelgl"
 	"golang.org/x/image/colornames"
@@ -12,22 +17,66 @@ func main() { // Directly firing the game from func main is not done, Run func i
 
 func run() {
 
-	cfg := pixelgl.WindowConfig{ // configuring the details of how our window would start
+	// configuring the details of how our window would start
+	cfg := pixelgl.WindowConfig{
 		Title:     "HRPG by Shayah-01",
-		Bounds:    pixel.R(0, 0, 250, 370),
+		Bounds:    pixel.R(0, 0, 360, 480),
 		Resizable: true,
 		VSync:     true,
 	}
-	win, err := pixelgl.NewWindow(cfg) // Opening the windows with prev. conf. - error if failed
-
-	if err != nil { //
-		panic(err) // Will print error & help me know what went wrong if window fails to open
+	// Opening the windows with prev. conf. - error if failed
+	win, err := pixelgl.NewWindow(cfg)
+	win.SetSmooth(true)
+	// Will print error & help me know what went wrong if window fails to open
+	if err != nil {
+		panic(err)
 	}
 
-	// !win.Closed() =  !win.JustPressed(pixelgl.KeyEscape) = closing with key esc or mouse close button
+	// Calling the image from the helperFunction
+	pic, err := loadPicture("sprites/pxArt.png")
+	if err != nil {
+		panic(err)
+	}
+	sprite := pixel.NewSprite(pic, pic.Bounds())
+	spritePosition := win.Bounds().Center()
 
-	for !win.Closed() && !win.JustPressed(pixelgl.KeyEscape) { // Keep windows updating for keypresses/events until as long as its NOT closed!
-		win.Clear(colornames.Rosybrown) // Window will display CLEAR darkgrey space
+	// Defining what will happen as long as the window stays open
+	// Keep windows updating for keypresses/events until as long as its NOT closed!
+	for !win.Closed() && !win.JustPressed(pixelgl.KeyEscape) {
+		win.Clear(colornames.Rosybrown)
+
+		// Only pixel.IM = "no spacial transformations" = static image
+		// .Scaled = resize & .Moved = position
+		sprite.Draw(win, pixel.IM.Scaled(pixel.ZV, 0.1).Moved(spritePosition))
+
+		// Configuring movements of characters START
+		if win.Pressed(pixelgl.KeyRight) {
+			spritePosition.X += 2.0
+		}
+		if win.Pressed(pixelgl.KeyUp) {
+			spritePosition.Y += 2.0
+		}
+		if win.Pressed(pixelgl.KeyLeft) {
+			spritePosition.X -= 2.0
+		}
+		if win.Pressed(pixelgl.KeyDown) {
+			spritePosition.Y -= 2.0
+		}
+		// Configuring movements of characters END
+
 		win.Update()
 	}
+}
+
+func loadPicture(path string) (pixel.Picture, error) { // helper func to load image
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	return pixel.PictureDataFromImage(img), nil
 }
